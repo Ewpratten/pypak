0dd67f7ab9b22e92fae10ba229bb7423cc20e759
Evan Pratten <ewpratten@gmail.com>
2019-02-01 22:02:31 -0500

    version

--- pypak/update.py
+++ pypak/update.py
@@ -1,7 +1,11 @@
 import urllib
 
+version_url = "https://raw.githubusercontent.com/Ewpratten/pypak/master/pypak/version.txt"
+
 def update(version):
 	print(f"Updating pypak to version: {version}")
 
 def checkUpdates():
 	print("Checking for updates")
+	version =  urllib.request.urlopen(version_url)
+	print(version)
