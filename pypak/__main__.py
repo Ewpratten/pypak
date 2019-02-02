99e5842e75f4e48443db6912fdbba6584802bfad
Evan Pratten <ewpratten@gmail.com>
2019-02-01 22:15:01 -0500

    changed version tracking

---
+++ pypak/__main__.py
@@ -0,0 +1,6 @@
+version = "0.0.1"
+# line above is parsed over http by the update script. Do not move it
+
+from update import update, checkUpdates
+
+checkUpdates(version)
--- pypak/update.py
+++ pypak/update.py
@@ -1,11 +1,14 @@
-import urllib
+import urllib.request
 
 version_url = "https://raw.githubusercontent.com/Ewpratten/pypak/master/pypak/version.txt"
 
 def update(version):
 	print(f"Updating pypak to version: {version}")
 
-def checkUpdates():
+def checkUpdates(version):
 	print("Checking for updates")
-	version =  urllib.request.urlopen(version_url)
-	print(version)
+	new_version =  urllib.request.urlopen(version_url).read().decode()
+	if version == new_version:
+		print("pypak up to date")
+		return
+	update(new_version)
